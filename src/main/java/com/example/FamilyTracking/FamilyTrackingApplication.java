cf353211254ef4061483b6eb95655e815236ac5c
GARFILD1000 <GARFILD1000@rambler.ru>
2019-05-09 02:09:50 +0700

    Created Spring Boot application with main hierarchy

---
+++ src/main/java/com/example/FamilyTracking/FamilyTrackingApplication.java
@@ -0,0 +1,15 @@
+package com.example.FamilyTracking;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FamilyTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyTrackingApplication.class, args);
+	}
+}
+
